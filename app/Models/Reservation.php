f37a3807bf3fb47684f65ec14bea1195d4646617
Shadow <skyreep@gmail.com>
2020-02-19 21:45:46 -0300

    add new model

---
+++ app/Models/Reservation.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Reservation extends Model
+{
+    //
+    protected $fillable = [
+        'user_id',
+        'room_id',
+        'num_of_guests',
+        'arrival',
+        'departure'
+    ];
+
+    public function room() {
+        return $this->belongsTo('App\Models\Room');
+        return $this->belongsTo('App\User');
+    }
+}
